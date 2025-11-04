88068bc8093786d11d0a2d0ebb6347e55971c255
Sam Lam <xlin67948gmail.com>
2025-11-04 20:14:29 +0800

    set up basic auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+"use client"
 import React from 'react'
+import { useAuth } from '@/contexts/AuthContext'
 
 const Home = () => {
+  const {loading,signInWithGoogle,user} = useAuth();
   return (
-    <div>Home</div>
+    <div>
+      <button className='text-black bg-amber-300 font-medium p-2 border-2 rounded-sm' disabled={loading} onClick={signInWithGoogle}>
+        {loading ? "Login ing..." : "Login"}
+      </button>
+    </div>
   )
 }
 
